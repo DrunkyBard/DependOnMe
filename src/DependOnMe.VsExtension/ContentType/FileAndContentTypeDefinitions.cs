0e425aa9e37fac73c454e94359d70be655ad6330
Sattar Imamov <sattar.imamov@gmail.com>
2018-05-14 23:44:46 +0200

    - Extend vsmanifest for vspro, add vspackage component - Fix dll references

---
+++ src/DependOnMe.VsExtension/ContentType/FileAndContentTypeDefinitions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.Utilities;
+using System.ComponentModel.Composition;
+
+namespace DependOnMe.VsExtension.ContentType
+{
+    internal static class FileAndContentTypeDefinitions
+	{
+		[Export]
+		[Name("hid")]
+		[BaseDefinition("text")]
+		internal static ContentTypeDefinition HidingContentTypeDefinition;
+
+		[Export]
+		[FileExtension(".hid")]
+		[ContentType("hid")]
+		internal static FileExtensionToContentTypeDefinition HiddenFileExtensionDefinition;
+	}
+}

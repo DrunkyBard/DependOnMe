c6f59d9101899a2faf298f9fd513233a63608698
Sattar Imamov <sattar.imamov@gmail.com>
2018-07-28 20:30:49 +0200

    - Some test code generation files

---
+++ src/DependOnMe.VsExtension/CodeGeneration/TestTemplate.User.cs
@@ -0,0 +1,16 @@
+﻿using CodeJam;
+
+namespace DependOnMe.VsExtension.CodeGeneration
+{
+    public partial class TestTemplate
+    {
+        public readonly TestTemplateModel Model;
+
+        public TestTemplate(TestTemplateModel model)
+        {
+            Code.NotNull(model, nameof(model));
+
+            Model = model;
+        }
+    }
+}
---
+++ src/DependOnMe.VsExtension/CodeGeneration/TestTemplateModel.cs
@@ -0,0 +1,77 @@
+﻿using CodeJam;
+using System.Collections.Generic;
+
+namespace DependOnMe.VsExtension.CodeGeneration
+{
+    public sealed class ModuleRegistration
+    {
+        public readonly string Name;
+
+        public ModuleRegistration(string name)
+        {
+            Code.NotNullNorWhiteSpace(name, nameof(name));
+
+            Name = name;
+        }
+    }
+
+    public sealed class DependencyRegistration
+    {
+        public readonly string Dependency;
+        public readonly string Implementation;
+
+        public DependencyRegistration(string dependency, string implementation)
+        {
+            Code.NotNullNorWhiteSpace(dependency, nameof(dependency));
+            Code.NotNullNorWhiteSpace(implementation, nameof(implementation));
+
+            Dependency = dependency;
+            Implementation = implementation;
+        }
+    }
+
+    public sealed class TestDefinition
+    {
+        public readonly string Name;
+        public readonly bool BoolFlag1Value;
+        public readonly bool BoolFlag2Value;
+        public readonly IReadOnlyCollection<DependencyRegistration> DependencyRegistrations;
+        public readonly IReadOnlyCollection<ModuleRegistration> ModuleRegistrations;
+
+        public TestDefinition(
+            string name, 
+            bool boolFlag1Value,
+            bool boolFlag2Value,
+            IReadOnlyCollection<DependencyRegistration> dependencyRegistrations, 
+            IReadOnlyCollection<ModuleRegistration> moduleRegistrations)
+        {
+            Code.NotNullNorWhiteSpace(name, nameof(name));
+            Code.NotNull(dependencyRegistrations, nameof(dependencyRegistrations));
+            Code.NotNull(moduleRegistrations, nameof(moduleRegistrations));
+
+            Name = name;
+            BoolFlag1Value = boolFlag1Value;
+            BoolFlag2Value = boolFlag2Value;
+            DependencyRegistrations = dependencyRegistrations;
+            ModuleRegistrations = moduleRegistrations;
+        }
+    }
+
+    public sealed class TestTemplateModel
+    {
+        public readonly string OutputFile;
+        public readonly string NamespaceDef;
+        public readonly IReadOnlyCollection<TestDefinition> Definitions;
+
+        public TestTemplateModel(string outputFile, string namespaceDef, IReadOnlyCollection<TestDefinition> definitions)
+        {
+            Code.NotNullNorWhiteSpace(outputFile, nameof(outputFile));
+            Code.NotNullNorWhiteSpace(namespaceDef, nameof(namespaceDef));
+            Code.NotNull(definitions, nameof(definitions));
+
+            OutputFile = outputFile;
+            NamespaceDef = namespaceDef;
+            Definitions = definitions;
+        }
+    }
+}
